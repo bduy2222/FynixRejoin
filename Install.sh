cc9f70cdd21584a2ba82c73bb6da6c3fbae53a15
bduy2222 <baoduyvn2222@gmail.com>
2026-04-18 16:09:13 +0700

    Add files via upload

--- Install.sh
+++ Install.sh
@@ -9,8 +9,16 @@ pkg update -y && pkg upgrade -y
 # ===== STORAGE =====
 termux-setup-storage
 
+# ===== WAKELOCK (🔥 QUAN TRỌNG) =====
+echo "[*] Enabling wakelock..."
+termux-wake-lock
+
+# ===== OPTIONAL: KEEP CPU ON (ANTI DOZE) =====
+# tránh android kill app khi idle
+settings put global stay_on_while_plugged_in 3 2>/dev/null
+
 # ===== INSTALL PACKAGES =====
-pkg install -y python python-pip clang make libffi openssl libjpeg-turbo libpng zlib freetype git cmake build-essential tsu libexpat-dev
+pkg install -y python python-pip clang make libffi openssl libjpeg-turbo libpng zlib freetype git cmake build-essential tsu libexpat-dev python-psutil
 
 # ===== PIP SETUP =====
 pip uninstall -y python-psutil 2>/dev/null
@@ -19,6 +27,6 @@ pip install --upgrade pip setuptools wheel
 
 pip install --prefer-binary \
 requests pytz pyjwt pycryptodome rich colorama flask pillow psutil discord.py python-socketio prettytable \
-|| CFLAGS="-Wno-error=implicit-function-declaration" pip install python-psutil
+|| CFLAGS="-Wno-error=implicit-function-declaration"
 
-echo "[DONE] Setup hoàn tất"
+echo "[DONE] Setup Done"
